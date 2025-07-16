d6286a38f028c70ef7b3cd09345f0506aeb5a559
Enrique Romero <enrique.romero1110@gmail.com>
2025-07-16 17:03:57 -0400

    Bot now registers test command

--- discorss.py
+++ discorss.py
@@ -1,17 +1,31 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 _ = load_dotenv()
 
 intents = discord.Intents.default()
+intents.message_content = True
 
-client = discord.Client(intents=intents)
+description = """A selfhosted RSS reader for discord"""
 
-# Here is where some code stuff goes apparently
-# refer to docs here: https://discordpy.readthedocs.io/en/stable/quickstart.html
+bot = commands.Bot(command_prefix="/", description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
 
 token = os.getenv("token")
 assert token is not None, "Please add token to .env file"
 
-client.run(token)
+bot.run(token)
