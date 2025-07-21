194638673ffd68813b1b169faf625785e9094a67
Enrique Romero <enrique.romero1110@gmail.com>
2025-07-20 20:40:57 -0400

    Somewhat get slash commands working

--- discorss.py
+++ discorss.py
@@ -10,7 +10,7 @@ intents.message_content = True
 
 description = """A selfhosted RSS reader for discord"""
 
-bot = commands.Bot(command_prefix="/", description=description, intents=intents)
+bot = commands.Bot(command_prefix="!", description=description, intents=intents)
 
 
 @bot.event
@@ -19,10 +19,35 @@ async def on_ready():
     print("------")
 
 
-@bot.command()
-async def add(ctx, left: int, right: int):
+@bot.hybrid_command()
+async def test(ctx):
+    await ctx.send("This is a hybrid command!")
+
+
+class AddFlags(commands.FlagConverter):
+    left: int = commands.flag(default=0, description="Number on the left")
+    right: int = commands.flag(default=0, description="Number on the right")
+
+
+@bot.hybrid_command()
+async def add(ctx, *, flags: AddFlags):
     """Adds two numbers together."""
-    await ctx.send(left + right)
+    await ctx.send(flags.left + flags.right)
+
+
+@bot.command()
+async def sync(ctx):
+    OWNER_USERID = os.getenv("userid")
+    if OWNER_USERID is None:
+        await ctx.send("Please configure user id in .env file")
+        return
+
+    if ctx.author.id != int(OWNER_USERID):
+        await ctx.send("You must be the owner to use this command!")
+        return
+
+    await bot.tree.sync()
+    await ctx.send("Command tree synced.")
 
 
 token = os.getenv("token")
